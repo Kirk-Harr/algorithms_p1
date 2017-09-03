44da36650588fd0f9678f32e66680ba64d943936
Kirk Harr <daehlie@gmail.com>
2017-09-03 15:45:03 -0700

    Implemented parts of Percolation data structure and added stubs for the PercolationStats class.

--- Percolation.java
+++ Percolation.java
@@ -1,28 +1,58 @@
 public class Percolation {
     // create n-by-n grid, with all sites blocked
-    public Percolation(int n) {}
+    private int[][] grid;
+    public Percolation(int n) {
+        grid = new int[n][n];
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                grid[i][j] = 0;
+            }
+        }
+    }
 
     // open site (row, col) if it is not open already
-    public void open(int row, int col) {}
+    public void open(int row, int col) {
+        if (grid[row][col] == 0){
+            grid[row][col] = 1;
+        }
+    }
 
     // is site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return true;
+        return grid[row][col] == 1;
     }
 
     // is site (row, col) full?
     public boolean isFull(int row, int col) {
-        return false;
+        return grid[row][col] == 0;
     }
 
     // number of open sites
     public int numberOfOpenSites() {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[i].length; j++){
+                if (grid[i][j] == 1){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
     // does the system percolate?
     public boolean percolates() {
         return false;
+    }
+
+    // Allow grid to be accessed
+    public int[][] getGrid() {
+        return grid;
+    }
+
+    // Not sure why we would ever use this...
+    public void setGrid(int[][] grid) {
+        this.grid = grid;
     }
 
     // test client (optional)
