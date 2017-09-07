0705d8ddfd77770b819b1828d35a673e2237160e
Kirk Harr <kharr@flightstats.com>
2017-09-07 14:41:22 -0700

    Updated logic of open method to properly handle all cases.

--- Percolation.java
+++ Percolation.java
@@ -6,10 +6,15 @@ public class Percolation {
     // create n-by-n grid, with all sites blocked
     private WeightedQuickUnionUF grid;
     private int gridSize;
+    private boolean[][] openSites;
+    private int top = 0;
+    private int bottom;
     public Percolation(int n) {
         if (n <= 0) throw new IndexOutOfBoundsException("grid size " + n + " out of bounds");
         gridSize = n;
         grid = new WeightedQuickUnionUF(n*n);
+        openSites = new boolean[n][n];
+        bottom = n*n-1;
     }
 
     // Private methods for utility functions
@@ -28,28 +33,38 @@ public class Percolation {
     public void open(int row, int col) {
         indexInBounds(row, col);
         int position = xyTo1D(row, col);
-        grid.union(position, 0);
+        openSites[row-1][col-1] = true;
+        if (row == 1) {
+            grid.union(xyTo1D(row, col), top);
+        }
+        if (row == gridSize) {
+            grid.union(xyTo1D(row, col), bottom);
+        }
+
+        if (col > 1 && isOpen(row, col - 1)) {
+            grid.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+        }
+        if (col < gridSize && isOpen(row, col + 1)) {
+            grid.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+        }
+        if (row > 1 && isOpen(row - 1, col)) {
+            grid.union(xyTo1D(row, col), xyTo1D(row - 1, col));
+        }
+        if (row < gridSize && isOpen(row + 1, col)) {
+            grid.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+        }
     }
 
     // is site (row, col) open?
     public boolean isOpen(int row, int col) {
         indexInBounds(row, col);
-        int position = xyTo1D(row, col);
-        return position != grid.find(position);
+        return openSites[row-1][col-1];
     }
 
     // is site (row, col) full?
     public boolean isFull(int row, int col) {
-        if (row == 1 && col == 1){
-            return true;
-        }
-        else {
-            int position = xyTo1D(row, col);
-            int leftNeighbor = xyTo1D(row, col - 1);
-            int rightNeighbor = xyTo1D(row, col + 1);
-            int upperNeighbor = xyTo1D(row - 1, col);
-            return (grid.connected(position, leftNeighbor) || grid.connected(position, rightNeighbor) || grid.connected(position, upperNeighbor));
-        }
+        indexInBounds(row, col);
+        return grid.connected(top, xyTo1D(row, col));
     }
 
     // number of open sites
@@ -59,39 +74,21 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        int i = 1;
-        boolean percolation = false;
-        boolean loopContinue = true;
-        while (i <=gridSize && loopContinue) {
-            for (int j = 1; j<=gridSize; j++){
-                if (i == gridSize) {
-                    if (isOpen(i,j) && isFull(i,j)) {
-                        percolation = true;
-                        i++;
-                        break;
-                    }
-                }
-                else if(isOpen(i,j) && isFull(i,j)){
-                    i++;
-                }
-                else if(j==gridSize){
-                    loopContinue = false;
-                }
-            }
-        }
-        return percolation;
+        return grid.connected(top, bottom);
     }
 
     // test client (optional)
     public static void main(String[] args) {
         Percolation p = new Percolation(3);
 
-        p.open(1,2);
+        p.open(2,1);
+        p.open(2,3);
+        p.open(3,1);
         p.open(2,2);
-        p.open(3,2);
+        p.open(1,3);
         System.out.println("Count is " + p.numberOfOpenSites());
 
-        System.out.println("Percolation is " + p.percolates());
+        System.out.println("(3,1) is full? " + p.isFull(3,1));
 
     }
 }
