24eea71313ecf25e609f99a2ce999b8750dedf9d
Kirk Harr <daehlie@gmail.com>
2017-09-16 16:11:47 -0700

    Completed implementation of Deque with proper double-linked list and added parameter checking and corner case handling.

--- Deque.java
+++ Deque.java
@@ -1,4 +1,7 @@
+import com.sun.org.apache.xml.internal.dtm.ref.DTMAxisIterNodeList;
+
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 
 public class Deque<Item> implements Iterable<Item> {
@@ -17,9 +20,16 @@ public class Deque<Item> implements Iterable<Item> {
 
         @Override
         public Item next() {
+            if (current == null)
+                throw new NoSuchElementException();
             Item item = current.s;
             current = current.next;
             return item;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
         }
     }
 
@@ -29,13 +39,19 @@ public class Deque<Item> implements Iterable<Item> {
 
     // construct an empty deque
     public Deque() {
-        first = new Node();
-        last = new Node();
-        first.next = last;
-        last.prev = first;
+        first = null;
+        last = null;
     }
 
+    private void checkItem(Item item) {
+        if (item == null)
+            throw new IllegalArgumentException();
+    }
 
+    private void checkSize() {
+        if (size == 0)
+            throw new NoSuchElementException();
+    }
     // is the deque empty?
     public boolean isEmpty() {
         return size == 0;
@@ -48,26 +64,46 @@ public class Deque<Item> implements Iterable<Item> {
 
     // add the item to the front
     public void addFirst(Item item) {
+        checkItem(item);
         Node n = new Node();
         n.s = item;
         n.next = first;
-        first.prev = n;
+        if (first != null)
+            first.prev = n;
         first = n;
         size++;
     }
 
     // add the item to the end
     public void addLast(Item item) {
+        checkItem(item);
         Node n = new Node();
         n.s = item;
-        last.next = n;
-        n.prev = last;
-        last = n;
-        size++;
+        if (first == null) {
+            first = n;
+            size++;
+            return;
+        }
+        else if (last == null){
+            last = n;
+            first.next = last;
+            last.prev = first;
+            size++;
+            return;
+        }
+        else {
+            Node l = last;
+            l.next = n;
+            n.prev = l;
+            last = n;
+            size++;
+        }
+
     }
 
     // remove and return the item from the front
     public Item removeFirst() {
+        checkSize();
         Node next = first.next;
         Node cur = first;
         next.prev = null;
@@ -78,6 +114,7 @@ public class Deque<Item> implements Iterable<Item> {
 
     // remove and return the item from the end
     public Item removeLast() {
+        checkSize();
         Node prev = last.prev;
         Node cur = last;
         prev.next = null;
@@ -103,5 +140,13 @@ public class Deque<Item> implements Iterable<Item> {
         while (iter.hasNext()) {
             System.out.println(iter.next());
         }
+        d.removeFirst();
+        d.removeLast();
+
+        iter = d.iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+
     }
 }
