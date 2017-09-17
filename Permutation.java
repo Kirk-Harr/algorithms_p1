e8c94f8ac3f5da69098faab491360784565c2d40
Kirk Harr <daehlie@gmail.com>
2017-09-17 15:04:12 -0700

    Completed Randomized Queue implementation with nasty hack for dequeue.

---
+++ Permutation.java
@@ -0,0 +1,17 @@
+import edu.princeton.cs.algs4.StdIn;
+
+import java.util.Iterator;
+
+public class Permutation {
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> queue = new RandomizedQueue<>();
+        while (!StdIn.isEmpty()) {
+            queue.enqueue(StdIn.readString());
+        }
+        Iterator<String> iter = queue.iterator();
+        for (int i = 0; i < k; i++) {
+            System.out.println(iter.next());
+        }
+    }
+}
--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -1,37 +1,122 @@
 import java.util.Iterator;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] s;
+    private int capacity = 10;
+    private int size = 0;
+
+    private class ArrayIterator implements Iterator<Item> {
+        private int index = 0;
+        private int[] randomIndexes = StdRandom.permutation(size);
+        @Override
+        public boolean hasNext() {
+            return index < randomIndexes.length;
+        }
+
+        @Override
+        public Item next() {
+            return s[randomIndexes[index++]];
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
     // construct an empty randomized queue
-    public RandomizedQueue() {}
+    public RandomizedQueue() {
+        s = (Item[]) new Object[capacity];
+    }
+
+    private void increaseCapacity() {
+        capacity = capacity*2;
+        Item[] k = (Item[]) new Object[capacity];
+        for(int i =0; i < size; i++){
+            k[1] = s[i];
+        }
+        s = k;
+    }
+
 
     // is the queue empty?
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     // return the number of items on the queue
     public int size() {
-        return 0;
+        return size;
     }
 
     // add the item
-    public void enqueue(Item item) {}
+    public void enqueue(Item item) {
+        if (size == capacity)
+            increaseCapacity();
+        s[size++] = item;
+    }
 
     // remove and return a random item
     public Item dequeue() {
-        return null;
+        int index;
+        if (size == 1) {
+            index = 0;
+        }
+        else {
+            index = StdRandom.uniform(size - 1);
+        }
+        Item i = s[index];
+        for (int j = index; j < size-1; j++){
+            s[j] = s[j+1];
+        }
+        size--;
+        return i;
     }
 
     // return (but do not remove) a random item
     public Item sample() {
-        return null;
+        int index = StdRandom.uniform(size-1);
+        return s[index];
     }
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
-        return null;
+        return new ArrayIterator();
     }
 
     // unit testing (optional)
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        RandomizedQueue<String> s = new RandomizedQueue<>();
+        s.enqueue("First");
+        s.enqueue("Second");
+        s.enqueue("Third");
+        s.enqueue("Fourth");
+        s.enqueue("Fifth");
+        s.enqueue("Sixth");
+        s.enqueue("Seventh");
+        s.enqueue("Eighth");
+        s.enqueue("Ninth");
+        s.enqueue("Tenth");
+
+        System.out.println(s.isEmpty());
+        System.out.println(s.dequeue());
+        Iterator<String> iter = s.iterator();
+        while (iter.hasNext())
+            System.out.print(iter.next()+ ": ");
+        System.out.println(s.dequeue());
+        System.out.println(s.dequeue());
+        System.out.println(s.dequeue());
+        System.out.println(s.dequeue());
+        iter = s.iterator();
+        while (iter.hasNext())
+            System.out.print(iter.next()+ ": ");
+        System.out.println(s.dequeue());
+        System.out.println(s.dequeue());
+        System.out.println(s.dequeue());
+        System.out.println(s.dequeue());
+        System.out.println(s.dequeue());
+        System.out.println(s.isEmpty());
+
+    }
 }
