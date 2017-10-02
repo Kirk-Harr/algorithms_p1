332792755f021c3617d501770542080ea5a3cdd8
Kirk Harr <daehlie@gmail.com>
2017-10-01 23:00:08 -0700

    Worked on further implementation of Board.

--- Board.java
+++ Board.java
@@ -1,27 +1,83 @@
+import java.util.Iterator;
+
 public class Board {
+    private int[] board;
+    private int n;
+
+    private class BoardIterable<Board extends Comparable<Board>> implements Iterable<Board> {
+        private Board[] boards;
+        private class BoardIterator implements Iterator<Board> {
+            int index;
+            public BoardIterator() {
+                index = 0;
+            }
+            @Override
+            public boolean hasNext() {
+                return index < (n*n)-1;
+            }
+
+            @Override
+            public Board next() {
+                return boards[index++];
+            }
+        }
+        @Override
+        public Iterator<Board> iterator() {
+            return new BoardIterator();
+        }
+
+    }
     // construct a board from an n-by-n array of blocks
     public Board(int[][] blocks) {
         // (where blocks[i][j] = block in row i, column j)
+        n = blocks[0].length;
+        board = new int[n*n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int pos = (i*n)+j;
+                board[pos] = blocks[i][j];
+            }
+        }
     }
 
     // board dimension n
     public int dimension() {
-        return 0;
+        return n;
     }
 
     // number of blocks out of place
     public int hamming() {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int pos = (i*n)+j;
+                if (board[pos] != 0 && board[pos] != pos+1)
+                    count++;
+            }
+        }
+        return count;
     }
 
     // sum of Manhattan distances between blocks and goal
     public int manhattan() {
-        return 0;
+        int total = 0;
+        for (int i = 0; i < board.length; i++) {
+            int cur = board[i];
+
+        }
+        return total;
     }
 
     // is this board the goal board?
     public boolean isGoal() {
-        return false;
+        boolean goal = true;
+        for (int i = 0; i < (n*n)-2; i++) {
+            if (board[i] != i+1)
+                goal = false;
+        }
+        if (board[(n*n)-1] != 0)
+            goal = false;
+        return goal;
     }
 
     // a board that is obtained by exchanging any pair of blocks
@@ -36,14 +92,28 @@ public class Board {
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        return null;
+        return new BoardIterable();
     }
 
     // string representation of this board (in the output format specified below)
     public String toString() {
-        return "";
+        String output = "";
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++) {
+                output = output.concat(board[(i*n)+j]+"  ");
+            }
+            output = output.concat(System.getProperty("line.separator"));
+        }
+        return output;
     }
 
     // unit tests (not graded)
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        int[][] test = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+        Board b = new Board(test);
+        System.out.println(b.dimension());
+        System.out.println(b.hamming());
+        System.out.println(b.isGoal());
+        System.out.println(b);
+    }
 }
