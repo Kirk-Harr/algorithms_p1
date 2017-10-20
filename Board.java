fa6d1f1f5cc84a7615a6456a64a0ddf89e6ea55e
Kirk Harr <kharr@flightstats.com>
2017-10-20 15:33:44 -0700

    Completed implementation of Board.

--- Board.java
+++ Board.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class Board {
@@ -5,19 +6,19 @@ public class Board {
     private int n;
 
 
-    // construct a board from an n-by-n array of blocks
-    public Board(int[][] blocks) {
-        // (where blocks[i][j] = block in row i, column j)
-        n = blocks[0].length;
-        board = new int[n][n];
+    // construct a board from an n-by-n array of board
+    public Board(int[][] board) {
+        // (where board[i][j] = block in row i, column j)
+        n = board[0].length;
+        this.board = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                board[i][j] = blocks[i][j];
+                this.board[i][j] = board[i][j];
             }
         }
     }
 
-    // Swap two blocks
+    // Swap two board
     private void swap(int i, int j, int i2, int j2) {
         int temp = board[i][j];
         board[i][j] = board[i2][j2];
@@ -36,7 +37,7 @@ public class Board {
         return n;
     }
 
-    // number of blocks out of place
+    // number of board out of place
     public int hamming() {
         int count = 0;
         for (int i = 0; i < n; i++) {
@@ -48,7 +49,7 @@ public class Board {
         return count;
     }
 
-    // sum of Manhattan distances between blocks and goal
+    // sum of Manhattan distances between board and goal
     public int manhattan() {
         int total = 0;
         for (int i = 0; i < n; i++) {
@@ -82,25 +83,65 @@ public class Board {
         return goal;
     }
 
-    // a board that is obtained by exchanging any pair of blocks
+    // a board that is obtained by exchanging any pair of board
     public Board twin() {
         Board dupe = new Board(board);
         int i = StdRandom.uniform(n-1);
         int j = StdRandom.uniform(n-1);
         int i2 = i;
-        int j2 = StdRandom.uniform(n-1);
+        int j2 = j+1;
         dupe.swap(i,j,i2,j2);
         return dupe;
     }
 
     // does this board equal y?
-    public boolean equals(Object y) {
-        return false;
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) return true;
+        if (other == null) return false;
+        if (other.getClass() != this.getClass()) return false;
+
+        Board that = (Board) other;
+        return (this.n == that.n) && (this.hamming() == that.hamming()) && (this.manhattan() == that.manhattan());
     }
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        return null;
+        Stack<Board> boards = new Stack<Board>();
+        int zeroRow = 0;
+        int zeroCol = 0;
+
+
+        if (zeroRow != 0) {
+            swap(zeroRow - 1, zeroCol, zeroRow, zeroCol);
+            //print(initboard);
+            boards.push(new Board(this.board));
+            // restore
+            swap(zeroRow - 1, zeroCol, zeroRow, zeroCol);
+        }
+
+        if (zeroRow < n - 1) {
+            swap(zeroRow + 1, zeroCol, zeroRow, zeroCol);
+            //print(initboard);
+            boards.push(new Board(this.board));
+            // restore
+            swap(zeroRow + 1, zeroCol, zeroRow, zeroCol);
+        }
+
+        if (zeroCol != 0) {
+            swap(zeroRow, zeroCol - 1, zeroRow, zeroCol);
+            boards.push(new Board(this.board));
+            // restore
+            swap(zeroRow, zeroCol - 1, zeroRow, zeroCol);
+        }
+
+        if (zeroCol < n - 1) {
+            swap(zeroRow, zeroCol + 1, zeroRow, zeroCol);
+            boards.push(new Board(this.board));
+            swap(zeroRow, zeroCol + 1, zeroRow, zeroCol);
+        }
+
+        return boards;
     }
 
     // string representation of this board (in the output format specified below)
@@ -117,9 +158,8 @@ public class Board {
 
     // unit tests (not graded)
     public static void main(String[] args) {
-        int[][] test = {{1, 3, 2}, {4, 6, 5}, {7, 0, 8}};
+        int[][] test = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
         Board b = new Board(test);
-        System.out.println(b.dimension());
         System.out.println(b.hamming());
         System.out.println(b.isGoal());
         System.out.println(b.manhattan());
@@ -129,5 +169,7 @@ public class Board {
         System.out.println(c.hamming());
         System.out.println(c.isGoal());
         System.out.println(c.manhattan());
+
+        System.out.println(b.equals(b));
     }
 }
